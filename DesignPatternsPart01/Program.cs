2388406151e68ea6f30b3c2358339527e5deaaa1
Tiago Barbosa <tiago@farias.cc>
2023-08-15 20:38:08 -0300

    organize codes.

--- DesignPatternsPart01/Program.cs
+++ DesignPatternsPart01/Program.cs
@@ -7,26 +7,23 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        // var icms = new ICMS();
-        // var iss = new ISS();
+        var icms = new ICMS();
+        var iss = new ISS();
+        var calculator = new TaxesCalculator();
+        var iccc = new ICCC();
+        var budget1 = new Budget(500);
+        var budget2 = new Budget(2300);
+        var budget3 = new Budget(3800);
 
 
-// calculator.PerformsCalculation(icms.GetType().Name, budget, icms);
-// calculator.PerformsCalculation(iss.GetType().Name, budget, iss);
+        calculator.PerformsCalculation(icms.GetType().Name, budget1, icms);
+        calculator.PerformsCalculation(iss.GetType().Name, budget1, iss);
 
-
-        // var iccc = new ICCC();
-        // var budget1 = new Budget(500);
-        // var budget2 = new Budget(2300);
-        // var budget3 = new Budget(3800);
-        //
-        // var calculator = new TaxesCalculator();
-        //
-        // calculator.PerformsCalculation(iccc.GetType().Name, budget1, iccc);
-        // calculator.PerformsCalculation(iccc.GetType().Name, budget2, iccc);
-        // calculator.PerformsCalculation(iccc.GetType().Name, budget3, iccc);
-        // Console.WriteLine("---");
-        // iccc.PerformsCalculation(iccc.GetType().Name, budget1, iccc);
+        calculator.PerformsCalculation(iccc.GetType().Name, budget1, iccc);
+        calculator.PerformsCalculation(iccc.GetType().Name, budget2, iccc);
+        calculator.PerformsCalculation(iccc.GetType().Name, budget3, iccc);
+        Console.WriteLine("---");
+        iccc.PerformsCalculation(iccc.GetType().Name, budget1, iccc);
 
         var investment = new MakeInvestment();
         var account = new Account();
@@ -35,7 +32,7 @@ internal class Program
         var bold = new Bold();
 
         account.Deposit(1000);
-        
+
         investment.Investment(account, moderate);
         investment.Investment(account, conservative);
         investment.Investment(account, bold);
---
+++ README.md
@@ -0,0 +1,5 @@
+# Learning Design Patterns - Part 01
+
+> Development
+
+Repository created for study some design patterns using C#.
